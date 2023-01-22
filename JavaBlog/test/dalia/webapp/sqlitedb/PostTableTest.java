d63e0d945da6844ba9be45518e185f5ab9c3f18c
Dalia-21 <dalia.sinclair@protonmail.com>
2023-01-22 16:43:31 +1100

    implement setup and teardown and first test case

--- JavaBlog/test/dalia/webapp/sqlitedb/PostTableTest.java
+++ JavaBlog/test/dalia/webapp/sqlitedb/PostTableTest.java
@@ -3,13 +3,15 @@ package dalia.webapp.sqlitedb;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class PostTableTest {
-	static PostTable testTable = new PostTable();
+	static PostTable postTable = new PostTable();
+	static UserTable userTable = new UserTable();
 	static String testUrl = "jdbc:sqlite:////home/dalia/git/JavaBlog/JavaBlog/db/test.db";
 	// test user variables
 	static User userPrototype = new User();
@@ -18,48 +20,66 @@ class PostTableTest {
 	static String passwordStub = "password";
 	static int userNum = 2;
 	// test post variables
-	static Post initialPost = new Post();
+	static Post postPrototype = new Post();
 	static Post testPost = new Post();
 	static String titleStub = "Test Post Number ";
 	static String bodyStub = "This is test post number ";
 	static Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 	static int numPosts = 6;
+	static int postId;
+	static int authorId;
 	
 	@BeforeAll
 	static void setUpBeforeClass() {
-		testTable.setConnectionUrl(testUrl);
-		testTable.createTable();
+		postTable.setConnectionUrl(testUrl);
+		postTable.createTable();
+		userTable.setConnectionUrl(testUrl);
+		userTable.createTable();
+		for (int i = 1; i <= userNum; i++) {
+			userPrototype.setUsername(usernameStub + String.valueOf(i));
+			userPrototype.setPassword(passwordStub + String.valueOf(i));
+			userTable.createUser(userPrototype);
+		}
+		for (int i = 1; i <= numPosts/2; i++) {
+			authorId = userTable.getUserByName(usernameStub + "1").getId();
+			postPrototype.setAuthorId(authorId);
+			postPrototype.setTitle(titleStub + String.valueOf(i));
+			postPrototype.setBody(bodyStub + String.valueOf(i));
+			postTable.createPost(postPrototype);
+		}
+		
+		for (int i = numPosts/2+1; i <= numPosts; i++) {
+			authorId = userTable.getUserByName(usernameStub + "2").getId();
+			postPrototype.setAuthorId(authorId);
+			postPrototype.setTitle(titleStub + String.valueOf(i));
+			postPrototype.setBody(bodyStub + String.valueOf(i));
+			postTable.createPost(postPrototype);
+		}
 		
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() {
-		testTable.dropTable();
-	}
-
-	@Test
-	void testCreatePost() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testDeletePost() {
-		fail("Not yet implemented");
+		postTable.dropTable();
+		userTable.dropTable();
 	}
 
 	@Test
 	void testGetPostById() {
-		fail("Not yet implemented");
+		// don't run right now
 	}
 
 	@Test
 	void testGetPostByAuthorId() {
-		fail("Not yet implemented");
+		ArrayList<Post> postsByUser1 = postTable.getPostByAuthorId(userTable.getUserByName(usernameStub + "1").getId());
+		assertEquals(numPosts/2, postsByUser1.size());
+		ArrayList<Post> postsByUser2 = postTable.getPostByAuthorId(userTable.getUserByName(usernameStub + "2").getId());
+		assertEquals(numPosts/2, postsByUser2.size());
 	}
 
 	@Test
 	void testUpdatePost() {
-		fail("Not yet implemented");
+		
 	}
 
 }

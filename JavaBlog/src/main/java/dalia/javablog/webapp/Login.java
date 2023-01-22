3a17f3a554bb30ea8dee00beb6ddae8e6c0a9007
Dalia-21 <dalia.sinclair@protonmail.com>
2023-01-22 20:12:40 +1100

    initial commit

---
+++ JavaBlog/src/main/java/dalia/javablog/webapp/Login.java
@@ -0,0 +1,53 @@
+package dalia.javablog.webapp;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Login
+ */
+
+@WebServlet(urlPatterns={"/login", "/login.html"})
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/HTML/login.html");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		PrintWriter out = response.getWriter();
+		out.println("<p>Hello " + username + ". Your password is " + password + "</p>");
+		out.close();
+	}
+
+}

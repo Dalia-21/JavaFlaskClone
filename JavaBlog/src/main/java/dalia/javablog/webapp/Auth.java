231cc142b4e701d724dd350b6ba49a3ed618bd7a
Dalia-21 <dalia.sinclair@protonmail.com>
2023-01-24 22:42:19 +1100

    remove unneeded import

--- JavaBlog/src/main/java/dalia/javablog/webapp/Auth.java
+++ JavaBlog/src/main/java/dalia/javablog/webapp/Auth.java
@@ -9,7 +9,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.MessageDigest;
